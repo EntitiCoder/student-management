67d54ec56706e9e64ef82cbd0e263e3bbb5f3c96
EntitiCoder <nhat.vn@tribox.me>
2024-11-14 11:38:31 +0800

    feat: clear all relations inside class when delete class

---
+++ prisma/migrations/20241114033710_cascade_in_class_relations/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "Assignment" DROP CONSTRAINT "Assignment_lessonId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Attendance" DROP CONSTRAINT "Attendance_lessonId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Media" DROP CONSTRAINT "Media_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Result" DROP CONSTRAINT "Result_studentId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Media" ADD CONSTRAINT "Media_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Result" ADD CONSTRAINT "Result_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attendance" ADD CONSTRAINT "Attendance_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE CASCADE ON UPDATE CASCADE;
